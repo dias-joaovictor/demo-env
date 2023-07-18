6d330d3ba7e6efe4f7d96a8478a6a92b27ff1cb8
João Victor Dias <kvj1610@hotmail.com>
2023-07-18 23:55:16 +0200

    Very Dummy project

--- src/main/java/com/example/demoenv/DemoEnvApplication.java
+++ src/main/java/com/example/demoenv/DemoEnvApplication.java
@@ -1,13 +1,30 @@
 package com.example.demoenv;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class DemoEnvApplication {
+public class DemoEnvApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoEnvApplication.class, args);
 	}
 
+	private final ConfigurableApplicationContext context;
+
+	@Value("${my.variable}")
+	private String myVariable;
+
+	public DemoEnvApplication(ConfigurableApplicationContext context) {
+		this.context = context;
+	}
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Hello World!");
+		System.out.println(myVariable);
+		context.close();
+	}
 }
