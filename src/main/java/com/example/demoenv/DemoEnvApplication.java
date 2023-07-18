29e1981c789ea98ad9a90595c5faf1adc32f3f32
João Victor Dias <kvj1610@hotmail.com>
2023-07-19 00:46:03 +0200

    Very Dummy project

--- src/main/java/com/example/demoenv/DemoEnvApplication.java
+++ src/main/java/com/example/demoenv/DemoEnvApplication.java
@@ -1,5 +1,6 @@
 package com.example.demoenv;
 
+import com.jcraft.jsch.Session;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
+import org.springframework.integration.file.remote.session.CachingSessionFactory;
+import org.springframework.integration.file.remote.session.SessionFactory;
+import org.springframework.integration.sftp.session.DefaultSftpSessionFactory;
+import org.springframework.integration.sftp.session.SftpRemoteFileTemplate;
+import org.springframework.integration.sftp.session.SftpSession;
+import com.jcraft.jsch.ChannelSftp;
 
 @SpringBootApplication
 public class DemoEnvApplication implements CommandLineRunner {
@@ -18,7 +25,16 @@ public class DemoEnvApplication implements CommandLineRunner {
 	private final ConfigurableApplicationContext context;
 
 	@Value("${my.variable}")
-	private String myVariable;
+	private String privateKey;
+
+	@Value("${sftp.host}")
+	private String host;
+
+	@Value("${sftp.user}")
+	private String user;
+
+	@Value("${sftp.port}")
+	private int port;
 
 	public DemoEnvApplication(ConfigurableApplicationContext context) {
 		this.context = context;
@@ -26,13 +42,41 @@ public class DemoEnvApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World!");
-		System.out.println(myVariable);
-		System.out.println(myVariable.length());
-		System.out.println(myVariable.split("\n").length);
+		System.out.println(privateKey);
+		System.out.println(host);
+		System.out.println(user);
+		System.out.println(port);
+		// System.out.println(privateKey.length());
+		// System.out.println(privateKey.split("\n").length);
 
-		Resource resource = new ByteArrayResource(myVariable.getBytes());
-		System.out.println(resource.contentLength());
-		System.out.println(resource.getFilename());
+		Resource resource = new ByteArrayResource(privateKey.getBytes());
+		// System.out.println(resource.contentLength());
+		// System.out.println(resource.getFilename());
+		tryToConnect();
 		context.close();
+	}
+
+	private void tryToConnect() {
+		DefaultSftpSessionFactory factory = new DefaultSftpSessionFactory();
+		factory.setUser(user);
+		if (privateKey != null) {
+			Resource resource = new ByteArrayResource(privateKey.getBytes());
+			factory.setPrivateKey(resource);
+		} else {
+			throw new IllegalArgumentException("HeyWorld SFTP password or private key is not found!");
+		}
+		factory.setHost(host);
+		factory.setPort(port);
+		factory.setAllowUnknownKeys(true);
+		SftpRemoteFileTemplate template = new SftpRemoteFileTemplate(factory);
+
+		try {
+			template.execute(session -> {
+				session.list(".");  // list files in the home directory
+				return null;
+			});
+		} catch (Exception ex) {
+			System.out.println("Failed to connect to SFTP server: " + ex.getMessage());
+		}
 	}
 }
